384010b4ff48065a2f7aa5b04c33e904ae518afd
RG-Mac <ramong5406@gmail.com>
2021-02-27 11:09:00 -0600

    created hello container

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from '@/components/HomePage';
+
+
+const routes = [
+  {
+    path: "/",
+    name: "HomePage",
+    component: HomePage,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
