3842c3c5d8db0d42f15a4eb2d613995a1a1d823e
RG-Mac <ramong5406@gmail.com>
2021-02-27 12:09:01 -0600

    changed background image

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,7 +2,14 @@
 <div class="containter-fluid m-0 p-0">
 
     <div id="home__page" class="full-height p-4">
-        Hello
+        <div class="container pt-3">
+            <div class="row pb-5">
+                <div class="col-lg-6">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+        </div>
     </div>
 
     <div id="about" class=" bg-warning p-4">
@@ -30,7 +37,7 @@ export default {
 <style scoped>
 #home__page {
     background-color: aqua;
-    background: url('../assets/mountain-and-lake.jpg') no-repeat center center;
+    background: url('../assets/16-bit-skyline.jpg') no-repeat center center;
     opacity: 90%;
     filter: brightness(75%);
     -webkit-background-size: cover;
